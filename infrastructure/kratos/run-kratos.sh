4be6d2bf8e31d0f6b68ecfb8fc8552b435a30c1b
wangsaiyu <wangsaiyu@cqu.edu.cn>
2021-12-20 22:01:44 +0800

    update kratos version

--- infrastructure/kratos/run-kratos.sh
+++ infrastructure/kratos/run-kratos.sh
@@ -5,9 +5,16 @@
 #  - MYSQL_HOST
 
 ##################################################
-# ensure kratos database and dsn                 #
+# generate kratos config file                    #
 ##################################################
 export DSN="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:3306)/kratos"
+
+cp kratos-without-dsn.yml .kratos.yaml
+echo "dsn: ${DSN}" >> ".kratos.yaml"
+
+##################################################
+# ensure kratos database and dsn                 #
+##################################################
 
 echo "$(mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SHOW DATABASES")" |grep -q "kratos";
 if [ $? -eq 0 ];
@@ -17,12 +24,6 @@ else
     mysql -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "CREATE DATABASE kratos"
     kratos migrate sql -e -y
 fi
-
-##################################################
-# generate kratos config file                    #
-##################################################
-cp kratos-without-dsn.yml .kratos.yaml
-echo "dsn: ${DSN}" >> ".kratos.yaml"
 
 ##################################################
 # run kratos                                     #
